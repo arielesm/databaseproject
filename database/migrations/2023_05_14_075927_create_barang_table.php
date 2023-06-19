c20cd0ab0bd54743ce1570f5482f2207ae2cef60
arielesm <136962158+arielesm@users.noreply.github.com>
2023-06-19 22:17:04 +0700

    Update 2023_05_14_075927_create_barang_table.php

--- database/migrations/2023_05_14_075927_create_barang_table.php
+++ database/migrations/2023_05_14_075927_create_barang_table.php
@@ -15,11 +15,31 @@ return new class extends Migration
     {
         Schema::create('viewbabsatu', function (Blueprint $table) {
             $table->id();
-            $table->text('')->nullable();
-            $table->text('')->nullable();
-            $table->text('')->nullable();
-            $table->text('')->nullable();
-            $table->text('')->nullable();
+            $table->text('nama_berkas')->nullable();
+            $table->text('judul_berkas')->nullable();
+            $table->text('nama_pt')->nullable();
+            $table->text('alamat_pt')->nullable();
+            $table->text('izinop_text')->nullable();
+            $table->text('izinop_file')->nullable();
+            $table->text('statakreinst_text')->nullable();
+            $table->text('statakreinst_file')->nullable();
+            $table->text('namaptmitra')->nullable();
+            $table->text('alamatptmitra')->nullable();
+            $table->text('negaramitra')->nullable();
+            $table->text('izinopmitra_text')->nullable();
+            $table->text('izinopmitra_file')->nullable();
+            $table->text('statakreinstmitra_text')->nullable();
+            $table->text('statakreinstmitra_file')->nullable();
+            $table->text('peringinter')->nullable();
+            $table->text('namaprodipt_select')->nullable();
+            $table->text('namaprodimitra_text')->nullable();
+            $table->text('akreprodpt_select')->nullable();
+            $table->text('akreprodmitra_text')->nullable();
+            $table->text('sk_akprodpt_file')->nullable();
+            $table->text('sk_akprodmitra_file')->nullable();
+            $table->text('izopeprodipt_file')->nullable();
+            $table->text('izopeprodimitra_file')->nullable();
+            $table->text('proposalusul_file')->nullable();
             $table->timestamps();
         });
     }
